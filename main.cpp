47c7f5604977be0483f14b3af3cbf7fe36d032db
yseungji <ysj9074_@naver.com>
2026-02-22 18:46:55 +0900

    스도쿠

---
+++ main.cpp
@@ -0,0 +1,165 @@
+//스도쿠
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> P;
+int sudoku[9][9] ;
+vector <P> jump;
+P next_index(int y, int x);
+void input();
+void possible_num (int y, int x, bool possible[]);
+void output();
+void recursion(int now);
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    input();
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(sudoku[i][j]==0){
+                jump.push_back(make_pair(i,j));
+                return 0;
+            }
+        }
+    }
+    recursion(0);
+
+    return 0;
+}
+
+
+void input(){
+    for(int i=0;i<9;i++){
+            string s;
+            cin >> s;
+            for(int j=0;j<9;j++){
+                sudoku[i][j] = s[j] - '0';
+            }
+        }
+    return;
+}
+
+void output() {
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            cout << sudoku[i][j];
+        }
+        cout << '\n';
+    }
+}
+
+P next_index(int y, int x){
+    P a;
+    if(y==8 && x == 8){
+        a.first = -1; a.second = -1;
+        return a;
+    }
+
+    if(x==8){
+        y=y+1;
+    }
+    else{
+        x= x+1;
+    }
+    a.first = y;
+    a.second = x;
+    return a;
+    
+}
+
+void possible_num (int y, int x, bool possible[]) {
+    typedef struct Case {
+        int xfrom;
+        int yfrom;
+        int xto;
+        int yto;
+    }Case;
+    for(int i=1;i<=9;i++) possible[i] = true; 
+    for(int i=0;i<9;i++) {  //같은 행에 있는 수 찾기
+        int fillnum = sudoku[y][i];
+        possible[fillnum] = false;
+    }
+    for(int i=0;i<9;i++) {  //같은 열에 있는 수 찾기
+        int fillnum = sudoku[i][x];
+        possible[fillnum] = false;
+    }
+    //3*3 섹터 있는 수 찾기
+    Case cc[9];
+    cc[0].xfrom=0; cc[0].xto=2;
+    cc[0].yfrom=0; cc[0].yto=2;
+    cc[1].xfrom=3; cc[1].xto=5;
+    cc[1].yfrom=0; cc[1].yto=2;
+    cc[2].xfrom=6; cc[2].xto=8;
+    cc[2].yfrom=0; cc[2].yto=2;
+    cc[3].xfrom=0; cc[3].xto=2;
+    cc[3].yfrom=3; cc[3].yto=5;
+    cc[4].xfrom=3; cc[4].xto=5;
+    cc[4].yfrom=3; cc[4].yto=5;
+    cc[5].xfrom=6; cc[5].xto=8;
+    cc[5].yfrom=3; cc[5].yto=5;
+    cc[6].xfrom=0; cc[6].xto=2;
+    cc[6].yfrom=6; cc[6].yto=8;
+    cc[7].xfrom=3; cc[7].xto=5;
+    cc[7].yfrom=6; cc[7].yto=8;
+    cc[8].xfrom=6; cc[8].xto=8;
+    cc[8].yfrom=6; cc[8].yto=8;
+
+    for(int k=0;k<9;k++){
+        if((y >= cc[k].yfrom && y<=cc[k].yto) && (x>=cc[k].xfrom && x<=cc[k].yto)){
+            for(int i=cc[k].yfrom;i<=cc[k].yto;i++){
+                for(int j=cc[k].xfrom;j<=cc[k].yto;j++){
+                    possible[sudoku[i][j]] = false;
+                }
+            }
+        }
+    }
+}
+
+void recursion(int now) {
+    if(now > jump.size()-1){
+        return;
+    }
+    int y,x;
+    y=jump[now].first; x = jump[now].second;
+    bool possible[9+1]; //1~9까지 가능한 수 찾기
+    possible_num(y,x, possible);
+    for(int i=1;i<=9;i++){
+        if(possible[i] == true) {
+            sudoku[y][x] = i;
+            output();
+            cout << '\n';
+            recursion(now+1);
+        }
+    }
+    P a = next_index(y,x);
+    if(a.first ==-1 && a.second==-1){
+        output();
+    }
+    
+    return;
+}
+
+
+//예제
+/*
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+
+100000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+000000000
+*/
